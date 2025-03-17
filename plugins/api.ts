34eb9adeca725543c616d3f03fe4da4b0eb39d03
Harriet Hall <harriet.hall@madetech.com>
2025-03-17 13:33:32 +0000

    update ip address

--- plugins/api.ts
+++ plugins/api.ts
@@ -2,7 +2,7 @@ export default defineNuxtPlugin((nuxtApp) => {
   // const { session } = useUserSession()
 
   const api = $fetch.create({
-    baseURL: 'http://18.171.182.97:5000',
+    baseURL: 'http://18.132.209.204:5000',
     // onRequest({ request, options, error }) {
     //   if (session.value?.token) {
     //     // note that this relies on ofetch >= 1.4.0 - you may need to refresh your lockfile
