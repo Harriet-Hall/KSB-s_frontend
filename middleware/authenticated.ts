6f4b96ffd18b0441db83667d340d85187ce68ea6
Harriet Hall <harriet.hall@madetech.com>
2025-03-25 15:29:44 +0000

    add login page with authentication logic

---
+++ middleware/authenticated.ts
@@ -0,0 +1,10 @@
+import { navigateTo } from "nuxt/app"
+
+export default defineNuxtRouteMiddleware(() => {
+  const { loggedIn } = useUserSession()
+
+  // redirect the user to the login screen if they're not authenticated
+  if (!loggedIn.value) {
+    return navigateTo('/login')
+  }
+})
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,22 @@
 
 <script setup>
-const { data: ksbs } = await useAPI('/ksbs')
+definePageMeta({
+  middleware: ['authenticated'],
+})
+const { user, clear: clearSession } = useUserSession()
 
+async function logout() {
+  await clearSession()
+  await navigateTo('/login')
+}
+const { data: ksbs } = await useAPI('/ksbs')
 </script>
+
 <template>
+  <div>
+    <h1>Welcome {{ user.name }}</h1>
+    <button @click="logout">Logout</button>
+  </div>
   <AddKsb/>
   <KsbList :data="ksbs"></KsbList>
 </template>
-
-
