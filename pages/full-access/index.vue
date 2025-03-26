ca357c7f1fae93e93ca96ace4da2ab51caeb690b
Harriet Hall <harriet.hall@madetech.com>
2025-03-26 20:54:08 +0000

    refactor variables to make constants

--- pages/full-access/index.vue
+++ pages/full-access/index.vue
@@ -4,16 +4,16 @@ import _ from "lodash";
 definePageMeta({
   middleware: ["authenticated"],
 });
-const { user, clear: clearSession } = useUserSession();
+const { clear: clearSession } = useUserSession();
 
 async function logout() {
   await clearSession();
   await navigateTo("../login");
 }
 
-let { data: ksbs } = await useAPI("/ksbs");
+const { data: ksbs } = await useAPI("/ksbs");
 
-let ksbList = ref(ksbs.value);
+const ksbList = ref(ksbs.value);
 
 const handleSortBy = async (property) => {
   return (ksbList.value = _.orderBy(ksbList.value, [property], ["asc"]));
