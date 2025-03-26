8e45dcd3fcaa09ec7f1a8c0f09ca1a6a72b1799c
Harriet Hall <harriet.hall@madetech.com>
2025-03-26 13:04:11 +0000

    remove commented out code

--- plugins/api.ts
+++ plugins/api.ts
@@ -1,22 +1,9 @@
 export default defineNuxtPlugin((nuxtApp) => {
-  // const { session } = useUserSession()
 
   const api = $fetch.create({
-    baseURL: 'http://18.132.209.204:5000',
-    // onRequest({ request, options, error }) {
-    //   if (session.value?.token) {
-    //     // note that this relies on ofetch >= 1.4.0 - you may need to refresh your lockfile
-    //     options.headers.set('Authorization', `Bearer ${session.value?.token}`)
-    //   }
-    // },
-    // async onResponseError({ response }) {
-    //   if (response.status === 401) {
-    //     await nuxtApp.runWithContext(() => navigateTo('/login'))
-    //   }
-    // }
-  })
+    baseURL: 'http://35.176.157.141:5000/',
 
-  // Expose to useNuxtApp().$api
+  })
   return {
     provide: {
       api
