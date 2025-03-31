1930756ac0059b607b87b9f74b00b6712b852c08
Harriet Hall <harriet.hall@madetech.com>
2025-03-31 12:36:05 +0100

    update admin routes

--- middleware/role.ts
+++ middleware/role.ts
@@ -2,7 +2,7 @@ import { navigateTo } from "nuxt/app"
 
 export default defineNuxtRouteMiddleware((to, from) => {
   const { user} = useUserSession()
-  const adminRoutes = ['/full-access/'];
+  const adminRoutes = ['/full-access', '/full-access/'];
 
   if (adminRoutes.includes(to.path) && user.value?.role !== 'admin') {
     return navigateTo('/restricted-access'); 
