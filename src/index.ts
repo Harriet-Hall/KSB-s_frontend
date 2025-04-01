c693976e2fdba4eef8e1c9dc76c9f6e5e11aa363
Harriet Hall <harriet.hall@madetech.com>
2025-04-01 08:02:16 +0100

    update api url

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { server } from "./mocks/node.ts";
 server.listen();
 
 async function app() {
-  const response = await fetch("http://35.176.157.141:5000/");
+  const response = await fetch("http://18.130.252.77:5000/");
   const user = await response.json();
   console.log(user);
 }
