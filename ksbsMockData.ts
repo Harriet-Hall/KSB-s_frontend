f64a23a077e64e895428e2a72b4d812aa02a8620
Harriet Hall <harriet.hall@madetech.com>
2025-04-07 18:03:21 +0100

    add ksbsMockData file to make ksbs global

---
+++ ksbsMockData.ts
@@ -0,0 +1,28 @@
+import type { Ksb } from "./types";
+
+export const ksbs :Ksb[] = [
+  {
+    id: "d9385487-94de-484b-8f0c-079d365815f9",
+    type: "Knowledge",
+    code: 2,
+    description: "knowledge description",
+    updated_at: "Wed, 12 Mar 2025 12:45:39 GMT",
+    theme: "code quality",
+  },
+  {
+    id: "d9385487-94de-484b-8f0c-079d365815f8",
+    type: "Skill",
+    code: 3,
+    description: "skill description",
+    updated_at: "Wed, 13 Mar 2025 12:45:39 GMT",
+    theme: "data persistence",
+  },
+  {
+    id: "d9385487-94de-484b-8f0c-079d365815f7",
+    type: "Behaviour",
+    code: 2,
+    description: "behaviour description",
+    updated_at: "Wed, 14 Mar 2025 12:45:39 GMT",
+    theme: "code quality",
+  },
+];
--- tests/ksblist.test.ts
+++ tests/ksblist.test.ts
@@ -4,38 +4,14 @@ import { renderSuspended } from "@nuxt/test-utils/runtime";
 import { screen } from "@testing-library/vue";
 import type { Ksb } from "../types.ts";
 import { mount } from "@vue/test-utils";
+import { ksbs } from "~/ksbsMockData.ts";
 
 describe("Ksb homepage", async () => {
   afterEach(() => {
     vi.clearAllMocks();
-  });
+  }); 
 
-  const MOCKED_DATA: Ksb[] = [
-    {
-      id: "d9385487-94de-484b-8f0c-079d365815f9",
-      type: "Knowledge",
-      code: 2,
-      description: "knowledge description",
-      updated_at: "Wed, 12 Mar 2025 12:45:39 GMT",
-      theme: "code quality",
-    },
-    {
-      id: "d9385487-94de-484b-8f0c-079d365815f8",
-      type: "Skill",
-      code: 3,
-      description: "skill description",
-      updated_at: "Wed, 13 Mar 2025 12:45:39 GMT",
-      theme: "data persistence",
-    },
-    {
-      id: "d9385487-94de-484b-8f0c-079d365815f7",
-      type: "Behaviour",
-      code: 2,
-      description: "behaviour description",
-      updated_at: "Wed, 14 Mar 2025 12:45:39 GMT",
-      theme: "code quality",
-    },
-  ];
+  const MOCKED_DATA: Ksb[] = ksbs
   it("should display a list of ksbs", async () => {
     await renderSuspended(KsbList, { props: { data: MOCKED_DATA } });
 
