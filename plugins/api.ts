a1fac4207ed5c9b9738f0d033e1c8340f0999b3c
Harriet Hall <harriet.hall@madetech.com>
2025-04-19 10:25:28 +0100

    update api url to new api gatway url

--- plugins/api.ts
+++ plugins/api.ts
@@ -1,7 +1,9 @@
 export default defineNuxtPlugin((nuxtApp) => {
 
   const api = $fetch.create({
-    baseURL: 'https://tf4uqz3vja.execute-api.eu-west-2.amazonaws.com/prod/',
+    baseURL: 'https://z6q58moouj.execute-api.eu-west-2.amazonaws.com/prod/',
+    // baseURL: 'http://18.132.36.228:5000/',
+    
   })
   return {
     provide: {
